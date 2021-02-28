04efd5a6675cb6ba89957bd22560c5bb0bb553e0
Valery780 <70340887+Valery780@users.noreply.github.com>
2021-02-28 14:37:09 +0200

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Class1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.Write("Введите ФИО: ");
+            name = Console.ReadLine();
+
+            Console.Write("Введите Возраст: ");
+            string age = Console.ReadLine();
+
+            Console.Write("Введите Адрес: ");
+            string adress = Console.ReadLine();
+
+            Console.Write("Введите Номер телефона: +380 ");
+            string phonenumber = Console.ReadLine();
+
+            Console.Write("Введите рост: ");
+            float height = float.Parse(Console.ReadLine());
+
+            Console.Write("Введите вес: ");
+            float weight = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"ФИО: {name}  " +
+                $"Возраст: {age} " +
+                $"Адрес: {adress} " +
+                $"Номер: +380 {phonenumber} " +
+                $"Рост: {height}м " +
+                $"Вес: {weight}кг");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Class2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите название продукта: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Введите количество: ");
+            float amount = float.Parse(Console.ReadLine());
+
+            Console.Write("Введите единицу измерения(кг, л, шт и тд.): ");
+            string unit = Console.ReadLine();
+
+            Console.Write("Введите цену продукта: ");
+            float cost = float.Parse(Console.ReadLine());
+
+
+            Console.WriteLine($"Название продукта: {name.ToUpper()}" +
+                $" Количество продукта: {amount}" +
+                $" Единица измерения: {unit.ToLower()}" +
+                $" Цена продукта: {cost}");
+
+
+
+
+        }
+    }
+}
